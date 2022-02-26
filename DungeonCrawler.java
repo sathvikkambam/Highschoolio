ce97cb8d130c1c3e51ed08d6f100855fcf982510
sathvikkambam <59803299+sathvikkambam@users.noreply.github.com>
2022-02-26 14:31:35 -0500

    Update DungeonCrawler.java

--- DungeonCrawler.java
+++ DungeonCrawler.java
@@ -1,31 +1,34 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 public class DungeonCrawler
 {
-    public static void main (String [][] args)
+    public static void main (String [] args)
     {
-        System.out.println("The year is 2073");
-        Thread.sleep(300);
-        System.out.println("You are in a spaceship traveling to the distant galaxy of NGC-3048");
-        Thread.sleep(300);
-        System.out.println("A large meteor crashes into the right side of the ship");
-        Thread.sleep(300);
-        System.out.println("The badly damaged ship crashes into the planet Xghrgd");
-        Thread.sleep(300);
-        System.out.println("Your only way to escape the hostile aliens is to reach the rocket launch site");
-        Thread.sleep(300);
-        System.out.println("As you crawl out the spacehsip you see an alien with a saber approaching you... ");
-        Thread.sleep(300);
-        System.out.println("LEVEL 1 - CRASH SITE");
-        Thread.sleep(300);
         
-        for(int i = 0; i<10; i++)
+        System.out.println("The year is 2073");
+        
+        System.out.println("You are in a spaceship traveling to the distant galaxy of NGC-3048");
+        
+        System.out.println("A large meteor crashes into the right side of the ship");
+        
+        System.out.println("The badly damaged ship crashes into the planet Xghrgd");
+        
+        System.out.println("Your only way to escape the hostile aliens is to reach the rocket launch site");
+        
+        System.out.println("As you crawl out the spacehsip you see an alien with a saber approaching you... ");
+        
+        System.out.println("LEVEL 1 - CRASH SITE");
+        
+        
+        for(char[] list : board.level1)
         {
-            for(int j = 0; j<10; j++)
+            for(char c : list)
             {
-                System.out.print(array[i][j]);
+                System.out.print(c);
             }
              System.out.println();
         }
-        System.out.println("# is the player/n! is the enemy\n$ is where the player needs to reach");
+        System.out.println("# is the player\n! is the enemy\n$ is where the player needs to reach\n? is a chest");
+        
     }   
 }
