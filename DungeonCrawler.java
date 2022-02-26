a1664b5733f83b50f06ec350a00338baf65bcb56
Karan5352 <100484217+Karan5352@users.noreply.github.com>
2022-02-26 13:14:30 -0500

    Update DungeonCrawler.java

--- DungeonCrawler.java
+++ DungeonCrawler.java
@@ -4,16 +4,18 @@ public class DungeonCrawler
     public static void main (String [][] args)
     {
         System.out.println("The year is 2073");
-        Thread.sleep(500);
+        Thread.sleep(300);
         System.out.println("You are in a spaceship traveling to the distant galaxy of NGC-3048");
-        Thread.sleep(500);
+        Thread.sleep(300);
         System.out.println("A large meteor crashes into the right side of the ship");
-        Thread.sleep(500);
+        Thread.sleep(300);
         System.out.println("The badly damaged ship crashes into the planet Xghrgd");
-        Thread.sleep(500);
+        Thread.sleep(300);
         System.out.println("Your only way to escape the hostile aliens is to reach the rocket launch site");
-        Thread.sleep(500);
+        Thread.sleep(300);
         System.out.println("As you crawl out the spacehsip you see an alien with a saber approaching you... ");
+        Thread.sleep(300);
+        System.out.println("LEVEL 1 - CRASH SITE");
         Thread.sleep(300);
         
     
