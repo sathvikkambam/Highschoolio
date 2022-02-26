5244140e7a628926ab0e6a768fb5df559ec270cb
sathvikkambam <59803299+sathvikkambam@users.noreply.github.com>
2022-02-26 14:14:07 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -28,24 +28,35 @@ public class Player
     public void combat(Enemy e)
     {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("What will you do? 0 for Light Attack. 1 for Heavy Attack. 2 for Block");
-        int act = keyboard.nextInt();
-        int enemyAct = (int)(Math.random()*3);
-        String[] actions = {"Heavy Attack","Light Attack","Block"};
-        System.out.println("You use " + actions[act] + " and the enemy uses " + actions[enemyAct]);
-        if(act == enemyAct)
+        while(enemy.health > 0 || stats[2] > 0)
         {
-            System.out.println("You both used the same move and don't lose any health");
+            System.out.println("What will you do? 0 for Light Attack. 1 for Heavy Attack. 2 for Block");
+            int act = keyboard.nextInt();
+            int enemyAct = (int)(Math.random()*3);
+            String[] actions = {"Heavy Attack","Light Attack","Block"};
+            System.out.println("You use " + actions[act] + " and the enemy uses " + actions[enemyAct]);
+            if(act == enemyAct)
+            {
+                System.out.println("You both used the same move and don't lose any health");
+            }
+            else if(enemyAct + 1 == act || enemyAct - 2 == act)
+            {
+                System.out.println("The enemy wins and you lose " + e.damage + " health");
+                stats[2] -= e.damage;
+            }
+            else
+            {
+                System.out.println("The you win and the enemy loses " + weapon.stat + " health");
+                e.health -= weapon.stat;
+            }
         }
-        else if(enemyAct + 1 == act || enemyAct - 2 == act)
+        if(enemy.health < 0)
         {
-            System.out.println("The enemy wins and you lose " + e.damage + " health");
-            stats[2] -= e.damage;
+            System.out.println("You have defeated the enemy");
         }
         else
         {
-            System.out.println("The you win and the enemy loses " + weapon.stat + " health");
-            e.health -= weapon.stat;
+            System.out.println("You have been defeated. Better luck next time.");
         }
     }   
 }
